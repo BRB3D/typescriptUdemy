1bdc2490f90c9cac4899e702c865186ff3190b97
BRB3D <samuraix221@hotmail.com>
2022-04-04 21:05:16 -0400

    typescript interface shows the error

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,19 @@ import axios from "axios";
 
 const url = `https://jsonplaceholder.typicode.com/todos/1`;
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then( res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+  const {id, title, completed} = todo;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `)
 });
