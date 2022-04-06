6b1ae6fc8d295d1a7032ac1af373a300eb59c51a
BRB3D <samuraix221@hotmail.com>
2022-04-05 22:01:53 -0400

    type date and kinds of types

---
+++ features/type.ts
@@ -0,0 +1,23 @@
+/**
+ * Different types of values in typescript
+ *  string
+ *  number  positive, negative, decimal points
+ *  boolean
+ *  Date
+ *  Todo
+ * 
+ * Two categories Primitive and Object
+ * Primitives           Object(Can trick typescript into thinking is a different type only ojbject types can do it)
+ * Number                     functions
+ * boolean                    arrays
+ * void                       classes
+ * undefined                  objects
+ * string 
+ * symbol
+ * null
+ */
+
+const today = new Date();
+today.getMonth;
+
+
