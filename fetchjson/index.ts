f6cddded17afc40816d16ff07afae7d4c6431389
BRB3D <samuraix221@hotmail.com>
2022-04-05 21:30:51 -0400

    Function creation with typescript

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -12,9 +12,13 @@ axios.get(url).then( res => {
   const todo = res.data as Todo;
   const {id, title, completed} = todo;
 
-  console.log(`
-    The Todo with ID: ${id}
-    Has a title of: ${title}
-    Is it finished? ${completed}
-  `)
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: String, completed: boolean) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}
+`);
+}
